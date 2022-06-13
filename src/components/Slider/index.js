ae2090ce4bed6b2e4c5aeb6e8caa86db671a2360
emanuel-hiroshi <emanuel.hiroshi@tecnoval.com.br>
2022-06-13 17:15:40 -0300

    Initial Commit

---
+++ src/components/Slider/index.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+
+export default function Slider({ setSliderLength, children }) {
+  useEffect(() => {
+    setSliderLength(children?.length);
+  });
+
+  return (
+    <div className="h-full w-full relative overflow-hidden">
+      {children.map((element, index) => element)}
+    </div>
+  );
+}
